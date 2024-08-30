f6b0a6277b9e5a6b562d76dd2f2770c109071b72
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-08-30 22:32:26 +0200

    Add files via upload

---
+++ src/Commands/Audios/confess.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+module.exports = {
+  usage: ["trailer"],
+  desc: "Display the bot's menu with categories and command details.",
+  commandType: "Audios",
+  isGroupOnly: false,
+  isAdminOnly: false,
+  isPrivateOnly: false,
+  emoji: "🇰🇷",
+  async execute(sock, m) {
+    try {
+      const menuAudioBuffer = "https://mustard-bird-6502.twil.io/assets/DRIEMO.mp3";
+      await buddy.sendAudio(m, menuAudioBuffer);
+
+    } catch (error) {
+      console.error("Error displaying menu:", error);
+      await buddy.reply(m, "An error occurred while displaying the menu. Please try again later.");
+    }
+  }
+};
