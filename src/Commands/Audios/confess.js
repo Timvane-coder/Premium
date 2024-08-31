259eaab1117677a65a4474b0d77389d1a3db9cd9
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-08-31 23:08:33 +0200

    Update confess.js

--- src/Commands/Audios/confess.js
+++ src/Commands/Audios/confess.js
@@ -12,6 +12,8 @@ module.exports = {
   emoji: "🇰🇷",
   async execute(sock, m) {
     try {
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/Menu2.jpeg');
+      const menuImageBuffer = fs.readFileSync(menuImagePath);
       const menuAudioBuffer = "https://mustard-bird-6502.twil.io/assets/DRIEMO.mp3";
       await buddy.sendAudio(m, menuAudioBuffer);
 
