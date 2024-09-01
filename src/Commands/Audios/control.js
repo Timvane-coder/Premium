7f28fbd1779e3cba41385cbeaf8afe9ea40e5185
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-01 12:43:57 +0200

    Update control.js

--- src/Commands/Audios/control.js
+++ src/Commands/Audios/control.js
@@ -3,18 +3,18 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["trailer"],
+  usage: ["controller"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Audios",
   isGroupOnly: false,
   isAdminOnly: false,
   isPrivateOnly: false,
-  emoji: "🇰🇷",
+  emoji: "🇲🇼",
   async execute(sock, m) {
     try {
       const menuImagePath = path.join(__dirname, '../../Assets/Menu/onesimus.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'controller by onesimus');
+      await buddy.sendImage(m, menuImageBuffer,'controller is a musical Audio done by a Malawian Afro Pop star Onesimus');
       
       const menuAudioBuffer = "https://mustard-bird-6502.twil.io/assets/Onesimus.mp3";
       await buddy.sendAudio(m, menuAudioBuffer);
