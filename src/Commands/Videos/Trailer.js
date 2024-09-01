4487b51259f1bbefd38a56fdf103fef37ff8b8bc
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 00:29:18 +0200

    Update Trailer.js

--- src/Commands/Videos/Trailer.js
+++ src/Commands/Videos/Trailer.js
@@ -3,26 +3,26 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["YouMe"],
+  usage: ["trailer_mv"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
   isAdminOnly: false,
   isPrivateOnly: false,
-  emoji: "🇰🇷",
+  emoji: "🇲🇼",
   async execute(sock, m) {
     try {
 
-      const menuImagePath = path.join(__dirname, '../../Assets/Menu/jennie.jpg');
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/driemo.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'You and Me is a record breaking kpop song by a South korean rapper Jennie Kim of Blackpink gir group band formed by YG entertainment');
+      await buddy.sendImage(m, menuImageBuffer,'Trailer is a Music Video by Malawian Singer Driemo signed under Van Ngongo Entertainment');
       
-      const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/YouMe.mp4";
-      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official You and Me Music Video');
+      const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/trailer.mp4";
+      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official Trailer Music Video');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while displaying the menu. Please try again later.");
+      await buddy.reply(m, "An error occurred while trying to Send the Video. Please type again #trailer_mv");
     }
   }
 };
