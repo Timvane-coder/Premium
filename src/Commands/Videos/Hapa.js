0ee0fbd3edc409c5a7279f5d2c4fb223de8cfcf4
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-01 23:49:17 +0200

    Update Hapa.js

--- src/Commands/Videos/Hapa.js
+++ src/Commands/Videos/Hapa.js
@@ -3,26 +3,26 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["YouMe"],
+  usage: ["Hapa"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
   isAdminOnly: false,
   isPrivateOnly: false,
-  emoji: "🇰🇷",
+  emoji: "🇲🇼",
   async execute(sock, m) {
     try {
 
-      const menuImagePath = path.join(__dirname, '../../Assets/Menu/jennie.jpg');
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/hapa.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'You and Me is a record breaking kpop song by a South korean rapper Jennie Kim of Blackpink gir group band formed by YG entertainment');
+      await buddy.sendImage(m, menuImageBuffer,'Hapa is a Music Video by a Talented Malawian Artist Emmie Deebo who is currently signed under Akometsi Entertainment,The Song features an Elite Tanzanian Artist Platform');
       
-      const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/YouMe.mp4";
-      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official You and Me Music Video');
+      const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/hapa.mp4";
+      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official Hapa Music Video');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while displaying the menu. Please try again later.");
+      await buddy.reply(m, "An error occurred while Sending Your requested Music Video. Please try again later.");
     }
   }
 };
