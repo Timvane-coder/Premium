103c9993d7e22ee0a81c5cc4690917d667060478
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 19:06:17 +0200

    Update bana.js

--- src/Commands/Audios/bana.js
+++ src/Commands/Audios/bana.js
@@ -3,26 +3,26 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["intoYou"],
+  usage: ["bana_pwanya"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Audios",
   isGroupOnly: false,
   isAdminOnly: false,
   isPrivateOnly: false,
-  emoji: "🇰🇷",
+  emoji: "🇲🇼",
   async execute(sock, m) {
     try {
 
-      const menuImagePath = path.join(__dirname, '../../Assets/Menu/');
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/kells.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'');
+      await buddy.sendImage(m, menuImageBuffer,'Bana Pwanya Is a Music Audio by a Malawian Singer Kell Kay Featuring Two Zambian Artist Yo Maps and Prince Luv.');
       
-      const menuAudioBuffer = "";
+      const menuAudioBuffer = "https://coral-ape-1798.twil.io/assets/banapwanya.mp3";
       await buddy.sendAudio(m, menuAudioBuffer);
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while Sending the Audio. Please type");
+      await buddy.reply(m, "An error occurred while Sending the Audio. Please type again #bana_pwanya ");
     }
   }
 };
