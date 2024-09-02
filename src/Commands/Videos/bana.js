b439a8ad2486a0e02a9f39b28cff9c4b30ba6635
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 19:36:14 +0200

    Update bana.js

--- src/Commands/Videos/bana.js
+++ src/Commands/Videos/bana.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["banapwanya"],
+  usage: ["bana_pwanya_mv"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
@@ -15,14 +15,14 @@ module.exports = {
 
       const menuImagePath = path.join(__dirname, '../../Assets/Menu/kellkay.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'bana pwanya is a musical Video by a Malawian Singer Kell Kay,the Song features two Zambian artists Yo Maps and Prince Luv');
+      await buddy.sendImage(m, menuImageBuffer,'Bana Pwanya is a Musical Video by a Malawian Singer Kell Kay,the Song Features two Zambian Artists Yo Maps and Prince Luv');
       
       const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/bana.mp4";
       await buddy.sendVideo(m, menuVideoBuffer,'WebGl Format* Bana Pwanya Official Music Video');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while displaying the menu. Please try again later.");
+      await buddy.reply(m, "An error occured while sending the video. Please type again #bana_pwanya_mv");
     }
   }
 };
