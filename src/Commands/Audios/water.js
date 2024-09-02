e2512723d69592cba243087512477c5013ae020c
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 19:16:16 +0200

    Update water.js

--- src/Commands/Audios/water.js
+++ src/Commands/Audios/water.js
@@ -3,26 +3,26 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["intoYou"],
+  usage: ["water"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Audios",
   isGroupOnly: false,
   isAdminOnly: false,
   isPrivateOnly: false,
-  emoji: "🇰🇷",
+  emoji: "🇿🇦",
   async execute(sock, m) {
     try {
 
-      const menuImagePath = path.join(__dirname, '../../Assets/Menu/');
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/water.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'');
+      await buddy.sendImage(m, menuImageBuffer,'Water is a Grammy Winning Music Audio by a South African Pop Diva Tyla');
       
-      const menuAudioBuffer = "";
+      const menuAudioBuffer = "https://coral-ape-1798.twil.io/assets/water.mp3";
       await buddy.sendAudio(m, menuAudioBuffer);
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while Sending the Audio. Please type");
+      await buddy.reply(m, "An error occurred while Sending the Audio. Please type again #water");
     }
   }
 };
