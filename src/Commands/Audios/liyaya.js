4e2488e7185a9d0a82cf8fe786c5efb19f2b8926
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 18:59:43 +0200

    Update liyaya.js

--- src/Commands/Audios/liyaya.js
+++ src/Commands/Audios/liyaya.js
@@ -1,1 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+module.exports = {
+  usage: ["intoYou"],
+  desc: "Display the bot's menu with categories and command details.",
+  commandType: "Audios",
+  isGroupOnly: false,
+  isAdminOnly: false,
+  isPrivateOnly: false,
+  emoji: "🇰🇷",
+  async execute(sock, m) {
+    try {
+
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/');
+      const menuImageBuffer = fs.readFileSync(menuImagePath);
+      await buddy.sendImage(m, menuImageBuffer,'');
+      
+      const menuAudioBuffer = "";
+      await buddy.sendAudio(m, menuAudioBuffer);
+
+    } catch (error) {
+      console.error("Error displaying menu:", error);
+      await buddy.reply(m, "An error occurred while Sending the Audio. Please type");
+    }
+  }
+};
 
