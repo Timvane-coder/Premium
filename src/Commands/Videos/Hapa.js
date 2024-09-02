3b7d9ce6cfc3a3349e046ba3b1408afb4ed164b1
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 19:32:45 +0200

    Update Hapa.js

--- src/Commands/Videos/Hapa.js
+++ src/Commands/Videos/Hapa.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["Hapa"],
+  usage: ["Hapa_mv"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
@@ -15,14 +15,14 @@ module.exports = {
 
       const menuImagePath = path.join(__dirname, '../../Assets/Menu/hapa.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'Hapa is a Music Video by a Talented Malawian Artist Emmie Deebo who is currently signed under Akometsi Entertainment,The Song features an Elite Tanzanian Artist Platform');
+      await buddy.sendImage(m, menuImageBuffer,'Hapa is a Music Video by a Talented Malawian Artist Emmie Deebo who is Currently Signed under Akometsi Entertainment,The Song Features an Elite Tanzanian Artist Platform');
       
       const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/hapa.mp4";
       await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official Hapa Music Video');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while Sending Your requested Music Video. Please try again later.");
+      await buddy.reply(m, "An error occurred while Sending the Video. Please type again #hapa_mv");
     }
   }
 };
