17bbd21b5a4e8b6d3089a940cdfc3d3b3afc744e
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 19:26:00 +0200

    Update tiyeni.js

--- src/Commands/Audios/tiyeni.js
+++ src/Commands/Audios/tiyeni.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
+module.exports = {
+  usage: ["ku_tiyeni"],
+  desc: "Display the bot's menu with categories and command details.",
+  commandType: "Audios",
+  isGroupOnly: false,
+  isAdminOnly: false,
+  isPrivateOnly: false,
+  emoji: "🇲🇼",
+  async execute(sock, m) {
+    try {
+
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/praise.jpg');
+      const menuImageBuffer = fs.readFileSync(menuImagePath);
+      await buddy.sendImage(m, menuImageBuffer,'Ku Tiyeni Is a Music Audio by a Malawian Singer Praise Umali Featuring Two  Artist Kell Kay and Zeze From Same Country.');
+      
+      const menuAudioBuffer = "https://coral-ape-1798.twil.io/assets/tiyeni.mp3";
+      await buddy.sendAudio(m, menuAudioBuffer);
+
+    } catch (error) {
+      console.error("Error displaying menu:", error);
+      await buddy.reply(m, "An error occurred while Sending the Audio. Please type again #Ku_tiyeni.");
+    }
+  }
+};
