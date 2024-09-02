bf029fda123160f92c6bb33a438006aefedda7fc
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 19:10:18 +0200

    Update hapa.js

--- src/Commands/Audios/hapa.js
+++ src/Commands/Audios/hapa.js
@@ -3,26 +3,26 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["intoYou"],
+  usage: ["hapa"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Audios",
   isGroupOnly: false,
   isAdminOnly: false,
   isPrivateOnly: false,
-  emoji: "🇰🇷",
+  emoji: "🇲🇼",
   async execute(sock, m) {
     try {
 
-      const menuImagePath = path.join(__dirname, '../../Assets/Menu/yuri.webp');
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/deebo.jpeg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'into you is an underated musical Audio done by a South korean Singer Yuri of Girls Generation Girl Group Band managed by SM entertainment');
+      await buddy.sendImage(m, menuImageBuffer,'Hapa is a Music Audio by a Female Malawian Artist Emmie Deebo,The Song Features a Tanzanian Artist Platform.');
       
-      const menuAudioBuffer = "https://coral-ape-1798.twil.io/assets/Yuri.mp3";
+      const menuAudioBuffer = "https://coral-ape-1798.twil.io/assets/hapa.mp3";
       await buddy.sendAudio(m, menuAudioBuffer);
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while displaying the menu. Please try again later.");
+      await buddy.reply(m, "An error occurred while Sending the Audio. Please type again #hapa.");
     }
   }
 };
