dd368b5ba7c6ef0441c74310dc81d4faeca98c5f
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-02 19:34:12 +0200

    Update water.js

--- src/Commands/Videos/water.js
+++ src/Commands/Videos/water.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["water"],
+  usage: ["water_mv"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
@@ -15,14 +15,14 @@ module.exports = {
 
       const menuImagePath = path.join(__dirname, '../../Assets/Menu/water.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer, 'Water is a musical Video by a South African grammy winning pop star Tyla');
+      await buddy.sendImage(m, menuImageBuffer, 'Water is a Musical Video by a South African Grammy Winning Pop Star Tyla');
       
       const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/Water.mp4";
       await buddy.sendVideo(m, menuVideoBuffer,'WebGl Format* Water official Musical Video');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
-      await buddy.reply(m, "An error occurred while displaying the menu. Please try again later.");
+      await buddy.reply(m, "An error occurred while Sending the Video. Please type again #water_mv");
     }
   }
 };
