e5b11681ba2eabd9e57e0a26eb5995aad1a2a812
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-09-04 19:01:23 +0200

    Update hailey.js

--- src/Commands/Videos/hailey.js
+++ src/Commands/Videos/hailey.js
@@ -17,7 +17,7 @@ module.exports = {
       const menuImageBuffer = fs.readFileSync(menuImagePath);
       await buddy.sendImage(m, menuImageBuffer,'Let Me Go is a song by American singer Hailee Steinfeld and Swedish record producer Alesso, featuring American country music duo Florida Georgia Line and American singer-songwriter Watt');
       
-      const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/LetMeGo.mp4";
+      const menuVideoBuffer = "https://download948.mediafire.com/06vn7cv5t8ygBJrTW2B4LeORgYoO16lx4wokPdHm6a9T0exz4a-rUWUTfhy0tqVt_Hz7h1ERi2EGMJ5RwaRHU3DuHNjBhwVloh5cP2fLH0wfNWIyJSYs6zkbZQDgRr6V2N6ggXh2_BocPMI89F81NW37Z7nRfIHDB_0A0wv8fWIpBv4/v4o2qnw5gckwj2v/VID-20240703-WA0001.mp4";
       await buddy.sendVideo(m, menuVideoBuffer,'WebGl Format* Let Me Go Official Musical Video');
 
     } catch (error) {
