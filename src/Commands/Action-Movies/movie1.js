908cc1aac2a1eeaef9540f3b3839705ccb41b38a
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-10-09 22:04:54 +0200

    Update movie1.js

--- src/Commands/Action-Movies/movie1.js
+++ src/Commands/Action-Movies/movie1.js
@@ -15,10 +15,10 @@ module.exports = {
 
       const menuImagePath = path.join(__dirname, '../../Assets/Menu/hailey.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'Shop for killers is a song by American singer Hailee Steinfeld and Swedish record producer Alesso, featuring American country music duo Florida Georgia Line and American singer-songwriter Watt');
+      await buddy.sendImage(m, menuImageBuffer,'Avatar the last Airbender is a film  by American');
       
-      const menuVideoBuffer = "https://download948.mediafire.com/w3rjzshb4uyg5NhXLo6N-MMJJIS3bMqazmMRifk6xyNa7Y2M3CEpsU872NKRFAwwOnRnhuM2meOpG4DiOhiBXSXGcJVP5OSgfmi0oF08RIUOlYz1w9BJ7kjj8Jytw_foBgbj5KZKOfSSOeD9LwdKriwT1yfSkY_CPvRFPWDFirr2PA/zeqmd9w7rv2yf11/Avatar_1A.mp4";
-      await buddy.sendVideo(m, menuVideoBuffer,'WebGl Format* Let Me Go Official Musical Video');
+      const menuVideoBuffer = "https://coral-ape-1798.twil.io/assets/Avatar_The_Last_Airbender_360P_S01_EP01(0).mp4";
+      await buddy.sendVideo(m, menuVideoBuffer,'WebGl Format* Avatar the  last Airbender Episode 1');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
