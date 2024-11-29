6f669698133d83c5b877f571eea2c4b9ef81d12e
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-11-29 21:54:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -273,7 +273,7 @@ function sendTshirtShop(recipientId) {
       attachment: {
         type: "template",
         payload: {
-          template_type: "list",
+          template_type: "generic",
           top_element_style: "compact",
           elements: [
             {
