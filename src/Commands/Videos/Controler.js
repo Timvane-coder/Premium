999b87ec4cca51995d7663be73f31a09dded6d75
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-12-19 14:40:34 +0200

    Update Controler.js

--- src/Commands/Videos/Controler.js
+++ src/Commands/Videos/Controler.js
@@ -3,22 +3,22 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["controler_mv"],
+  usage: ["apt_mv"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
   isAdminOnly: false,
   isPrivateOnly: false,
-  emoji: "🇲🇼",
+  emoji: "🇰🇷",
   async execute(sock, m) {
     try {
 
-      const menuImagePath = path.join(__dirname, '../../Assets/Menu/controla.jpg');
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/rose.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'Controler is a Music Video by a Malawian Afro-Pop star Onesimus');
+      await buddy.sendImage(m, menuImageBuffer,'Apt is a Music Video by American singer Bruno Mars and a South Korean K-Pop star Rose who is also a member of a girl band Blackpink');
       
       const menuVideoBuffer = "https://mustard-bird-6502.twil.io/assets/Rose.mp4";
-      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official Controler Music Video');
+      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official Apt Music Video');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
