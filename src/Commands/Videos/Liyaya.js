d45b4ffc8e7a74bca7fefa0ae71351afe632a03e
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-12-19 14:42:15 +0200

    Update Liyaya.js

--- src/Commands/Videos/Liyaya.js
+++ src/Commands/Videos/Liyaya.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["Liyaya_mv"],
+  usage: ["busy_mv"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
@@ -13,12 +13,12 @@ module.exports = {
   async execute(sock, m) {
     try {
 
-      const menuImagePath = path.join(__dirname, '../../Assets/Menu/liyaya.jpg');
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/aika.jpg');
       const menuImageBuffer = fs.readFileSync(menuImagePath);
-      await buddy.sendImage(m, menuImageBuffer,'Liyaya is a Music Video done by a UK-based Malawian Artist Hazel Mak');
+      await buddy.sendImage(m, menuImageBuffer,'budy  is a Music Video done by a  Malawian Artist Lady Aika');
       
       const menuVideoBuffer = "https://mustard-bird-6502.twil.io/assets/Aika.mp4";
-      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official Liyaya Music Video');
+      await buddy.sendVideo(m, menuVideoBuffer,'WebGl format* Official Busy Music Video');
 
     } catch (error) {
       console.error("Error displaying menu:", error);
