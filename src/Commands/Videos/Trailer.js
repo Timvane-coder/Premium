1f7d85abd410c28a0a528cca5e5e49786e82f8ce
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-12-19 14:45:51 +0200

    Update Trailer.js

--- src/Commands/Videos/Trailer.js
+++ src/Commands/Videos/Trailer.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["trailer_mv"],
+  usage: [""],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Videos",
   isGroupOnly: false,
