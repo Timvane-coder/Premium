57bb63934552d1dbf4197aeadeb7f940c3b3c985
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-12-20 10:07:39 +0200

    Update heeriye.js

--- src/Commands/Audios/heeriye.js
+++ src/Commands/Audios/heeriye.js
@@ -3,7 +3,7 @@ const path = require('path');
 const os = require('os');
 
 module.exports = {
-  usage: ["mantra"],
+  usage: ["heeriye"],
   desc: "Display the bot's menu with categories and command details.",
   commandType: "Audios",
   isGroupOnly: false,
