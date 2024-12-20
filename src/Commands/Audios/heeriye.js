875db8f7e40db5ed055d71217ed06ea80298598b
Timvane-coder <127635999+Timvane-coder@users.noreply.github.com>
2024-12-20 10:07:01 +0200

    Create heeriye.js

---
+++ src/Commands/Audios/heeriye.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
+module.exports = {
+  usage: ["mantra"],
+  desc: "Display the bot's menu with categories and command details.",
+  commandType: "Audios",
+  isGroupOnly: false,
+  isAdminOnly: false,
+  isPrivateOnly: false,
+  emoji: "🇮🇳",
+  async execute(sock, m) {
+    try {
+
+      const menuImagePath = path.join(__dirname, '../../Assets/Menu/mantra.jpg');
+      const menuImageBuffer = fs.readFileSync(menuImagePath);
+      await buddy.sendImage(m, menuImageBuffer,'Mantra Is a Music Audio by a South Korean K-pop singer Jennie Kim who is also a member of blackpink');
+      
+      const menuAudioPath = path.join(__dirname, '../../Assets/Menu/mantra.mp3');
+      const menuAudioBuffer = fs.readFileSync(menuAudioPath);
+      await buddy.sendAudio(m, menuAudioBuffer);
+
+    } catch (error) {
+      console.error("Error displaying menu:", error);
+      await buddy.reply(m, "An error occurred while Sending the Audio. Please type again #bana_pwanya ");
+    }
+  }
+};
